49f7f4d0ff959386ac8a315ca4381ae408b7f640
Juan Balbuena <juanbalb48@gmail.com>
2023-11-19 16:20:32 -0600

    added the Form component to the App.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import Form from '../Form/Form.jsx';
 
 function App() {
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
+      {/* <input placeholder='Airline Name' />
+      <button>Add Airline</button> */}
+      <Form />
       <table>{/* Airlines should be listed here */}</table>
     </div>
   );
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,28 @@
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 
 
     function Form(){
+        const [airlines, setAirlineName] = useState("");
 
+        const dispatch = useDispatch();
+
+        const handleSubmit = e => {
+            e.preventDefault();
+            const action = {
+                type: 'ADD_AIRLINE', payload: {
+                    airlines: airlines,
+                }
+            };
+            dispatch(action);
+        };
+        return (
+            <form onSubmit={handleSubmit}>
+                <input value={airlines} onChange={e => setAirlineName(e.target.value)} />
+                <button type="submit">Add Airline</button>
+            </form>
+        )
 }
 
 
